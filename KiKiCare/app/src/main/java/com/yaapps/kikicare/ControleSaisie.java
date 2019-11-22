361c5efe63b2ba175864935b940433faf197f0dd
Abdelaziz Mezri <abdelaziz.mezri@esprit.tn>
2019-11-22 16:56:07 +0100

    Class ControleSaisie

---
+++ KiKiCare/app/src/main/java/com/yaapps/kikicare/ControleSaisie.java
@@ -0,0 +1,55 @@
+package com.yaapps.kikicare;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ControleSaisie {
+
+    private static Matcher matcher;
+
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    private static final String pwd=  "^[A-Za-z0-9]+$";
+    private static Pattern pattern1 = Pattern.compile(pwd);
+
+    public static boolean isString(String text) {
+        return text.matches("^[a-zA-Z]+$");
+    }
+
+    public static boolean isNull(String text){
+        return text.equals(""); //null
+//n'est pas vide
+    }
+
+    public static boolean isUsername(String text) {
+        return text.matches("^[A-Za-z0-9]+$+");
+
+    }
+
+    public static boolean DateNullCS(String date){
+        return date.equals("");
+    }
+
+    public static boolean adresse(String text) {
+        return text.matches("^[A-Z a-z 0-9]+$");
+    }
+
+    public static boolean isCin(String text) {
+        return text.matches("^[0-9]+$") && text.length() == 8;
+    }
+
+    public static boolean isTel(String text) {
+        return text.matches("^[0-9]+$") && text.length() == 8;
+    }
+
+    public static boolean validEmail(final String hex) {
+        matcher = pattern.matcher(hex);
+        return matcher.matches();
+    }
+
+    public static boolean validPasswor(final String hex) {
+        matcher = pattern1.matcher(hex);
+        return matcher.matches();
+    }
+
+}
